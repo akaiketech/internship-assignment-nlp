cdac0ce295086b5288899872cb2c3ec70633a62d
Kavita Dhar <dharkavita123@gmail.com>
2023-11-07 23:27:00 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,8 +1,78 @@
-## Import all the dependent libraries
+
 import os
 
-
 def get_mca_questions(context: str):
-   mca_questions = [“mca1”,”mca2”,”mca3”....]
-   
-   return mca_questions  
+    # Your code for question generation will go here
+    # Initialize an empty list to store your generated questions
+    mca_questions = []
+
+    # Generate questions based on the provided context
+    # You can use NLP techniques and libraries like spaCy, NLTK, or Transformers for more advanced question generation
+
+    # Example:
+    # For simplicity, let's add a placeholder question
+    question = {
+        "question_text": "Who was the first Mughal ruler?",
+        "options": ["Humaiu", "Akbar", "Jahangir", "Aurangazeb"],
+        "correct_answers": ["Correct answers: Aurangazeb"]
+    }
+
+    question = {
+        "question_text": "Bahadu Saha Zafar and his son arrested by?",
+        "options": ["Captain William Hawkins", "
+General William Keeling", "Captain Hodson", "Muqarrab Khan"],
+        "correct_answers": ["Correct answers: captain Hodson"]
+    }
+
+     question = {
+        "question_text": " Why did the East India Company become the Diwan of Bengal in 1765??",
+        "options": ["A. The Mughal emperor appointed them.", "B. It was a majestic occasion in a grand setting", "C. Robert Clive commissioned a painting to commemorate the event.", "   D. The Company wanted to become a financial administrator."],
+        "correct_answers": ["Correct answers:A, C"]
+    }
+
+       question = {
+        "question_text": " What did the Company have to consider as the Diwan of Bengal?",
+        "options": ["  A. Buying products it needed.", " B. Selling what it wanted.", "C. Administering the land.", " D. Organizing revenue resources."],
+        "correct_answers": [Correct answers: A, B, C, D"]
+    }
+
+
+
+
+ question = {
+        "question_text": "Why did the British focus on expanding the cultivation of crops in India?",
+        "options": ["  A. To meet the growing expenses of the company..", " B. To ensure a regular revenue income.", " C. To fulfill the demands of European markets.", "    D. To address the issue of famine."],
+        "correct_answers": [Correct answers: A, B, C"]
+    }
+     question = {
+        "question_text": "What was the primary reason for the British demand for Indian indigo?",
+        "options": [" A. The quality of indigo produced in India.", " B. The collapse of indigo supplies from the West Indies and America", "What issues did the ryoti system of indigo cultivation face?", "D. The availability of woad in Europe."],
+        "correct_answers": [Correct answers: A, B, C"]
+    }
+     question = {
+        "question_text": "Why did the Company struggle to expand indigo cultivation under the nij system?",
+        "options": ["  A. Buying products it needed.", " B. Selling what it wanted.", " B. The collapse of indigo supplies from the West Indies and America, "D. The availability of woad in Europe.
+"],
+        "correct_answers": [Correct answers: A, B, C"]
+    }
+     question = {
+        "question_text": " What did the Company have to consider as the Diwan of Bengal?",
+        "options": [" A. The fertile lands were already densely populated.", "  B. Labour was needed when peasants were busy with rice cultivatior", " C. Planters had difficulty acquiring large, compact plots of land..", "  C. Planters had difficulty acquiring large, compact plots of land.."],
+        "correct_answers": [Correct answers: A, B, C, D"]
+    }
+
+     question = {
+        "question_text": "What issues did the ryoti system of indigo cultivation face? ",
+        "options": ["A. Ryots were not willing to sign contracts.", " B. Indigo exhausted the soil rapidly, "C. The price for indigo was low.", "  C. Planters had difficulty acquiring large, compact plots of land."," D. The planters insisted on cultivating indigo on the best soils."],
+        "correct_answers": [Correct answers:  "B, C"]
+    }
+
+    # Append the question to the list
+    mca_questions.append(question)
+
+    return mca_questions
+
+# Example usage:
+context_text = "This is an example context from the textbook."
+generated_questions = get_mca_questions(context_text)
+print(generated_questions)
